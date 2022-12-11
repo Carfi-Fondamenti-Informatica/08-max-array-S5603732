8fcb3caa6f24f39d6d4e65afc585a3adbf91efce
S5603732 <118397147+S5603732@users.noreply.github.com>
2022-12-11 19:24:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "primalibreria.h"
+
+using namespace std;
 
 int main(){
+  int a=0;
+    float b, d=0;
+    cin >>a;
+    float c[a];
+    for (int i=0; i<a; i++) {
+        cin >> b;
+        c[a]=b;
+        if (b>d) d=b;
+    }
+    cout << array(d);
   return 0;
 }
